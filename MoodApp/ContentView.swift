958446829b45285e54ffd287691f52ab21a2556d
Angela Drees <drees.angie@gmail.com>
2022-12-06 15:19:44 -0600

    Initial Commit

---
+++ MoodApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MoodApp
+//
+//  Created by Gavin's Old Mac on 12/6/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MoodApp/MoodAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoodAppApp.swift
+//  MoodApp
+//
+//  Created by Gavin's Old Mac on 12/6/22.
+//
+
+import SwiftUI
+
+@main
+struct MoodAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
